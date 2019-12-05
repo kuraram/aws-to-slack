ee2b9fe3ae3296365a2b63f4b4d5ba7c78546457
kuraram <p232034m@mail.kyutech.jp>
2019-12-05 19:38:37 +0900

    modify

--- lambda_function.rb
+++ lambda_function.rb
@@ -8,10 +8,10 @@ def codebuild_handler(event:, context:)
 
 	puts "=== event ===\n #{event}\n ==="	# Display event
 	project = event["detail"]["project-name"]
-    status = event["detail"]["build-status"]
+  status = event["detail"]["build-status"]
 	additional_info = event["detail"]["additional-information"]
-    build_id = additional_info["logs"]["stream-name"]
-    log_link = additional_info["logs"]["deep-link"]
+  build_id = additional_info["logs"]["stream-name"]
+  log_link = additional_info["logs"]["deep-link"]
 	phases = additional_info["phases"]
 	phase_txt = ""
 	phases.each do | phase |
@@ -27,18 +27,19 @@ def codebuild_notifer(build_id, project, status, phase_txt, link)
 
 	uri  = URI.parse(ENV["URL"])
 	params = { 	# Create payload
-		text: get_text(status, project), 
-		channel: ENV["CHANNEL"],
-		"attachments":[{
-			author_name: "#{project} in codebuild",
-			author_link: "https://ap-northeast-1.console.aws.amazon.com/codesuite/codebuild/projects/#{project}/history",
-			text: phase_txt,
-			color: get_color(status),
-			actions: [{"type": "button", "text": "view log", "url": link}]
-		}]
-		}
+      text: get_text(status, project), 
+      channel: ENV["CHANNEL"],
+      "attachments":[{
+        author_name: "#{project} in codebuild",
+        author_link: "https://ap-northeast-1.console.aws.amazon.com/codesuite/codebuild/projects/#{project}/history",
+        text: phase_txt,
+        color: get_color(status),
+        actions: [{"type": "button", "text": "view log", "url": link}]
+      }]
+    }
+    
 	http = Net::HTTP.new(uri.host, uri.port)
-	http.use_ssl = true
+  http.use_ssl = true
 	http.start do
 	  request = Net::HTTP::Post.new(uri.path)
 	  request.set_form_data(payload: params.to_json)
@@ -57,9 +58,9 @@ def get_text(status, project)
 end
 
 def get_color(status)
-    if status == "FAILED"
-        return "danger"
-    else
-		return "good"
+  if status == "FAILED"
+    return "danger"
+  else
+    return "good"
 	end
 end
