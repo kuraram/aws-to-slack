95e753050d1a4ed22e1c4d295e5a61d25c6817ca
kuraram <p232034m@mail.kyutech.jp>
2020-02-17 13:59:27 +0900

    implement lb notification

--- statuscode/lambda_function.rb
+++ statuscode/lambda_function.rb
@@ -6,13 +6,29 @@ require "json"
 
 def lambda_handler(event:, context:)
 
-  #puts "=== event ===\n #{event}\n ==="	# Display event
-  
+  puts "=== event ===\n #{event}\n ==="	# Display event
+  sns = event["Records"][0]["Sns"]
+  subject = sns["Subject"]
+  timestamp = sns["Timestamp"]
+  phase_txt = ""
+ 
+  lb_notifer(sns, subject, timestamp, phase_txt)
+
+end
+
+def lb_notifer(sns, subject, timestamp, phase_txt)
+
   uri  = URI.parse(ENV["URL"])
   params = { 	# Create payload
-    text: event.to_s,
-    channel: ENV["CHANNEL"]
+    text: subject.to_s, 
+    channel: ENV["CHANNEL"],
+    "attachments":[{
+      author_name: "ALB Console",
+      author_link: "https://ap-northeast-1.console.aws.amazon.com/ec2/v2/home?region=ap-northeast-1#LoadBalancers:sort=loadBalancerName",
+      color: "danger",
+    }]
   }
+
   http = Net::HTTP.new(uri.host, uri.port)
   http.use_ssl = true
   http.start do
